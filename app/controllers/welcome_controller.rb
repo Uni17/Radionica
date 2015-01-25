0320228e37d98dfdef7ef51905dd393f3fd89c63
Una Gasparovic <rruuna@gmail.com>
2015-01-25 16:25:22 +0100

    first-commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  def index
+  	@welcome = "Hello World!"
+  end
+  
+  def hello
+
+  end
+  
+end
---
+++ app/views/songs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @song, :id, :title, :year, :author, :year, :created_at, :updated_at
